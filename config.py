7aa9fb598d50c6fccc14fc657a76a036ca8db7bc
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-06-25 21:29:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-from os import getenv
+from os import environ
 from dotenv import load_dotenv, find_dotenv, dotenv_values
 
 try:
     load_dotenv(find_dotenv())
 except:
     raise
-Config = dotenv_values(find_dotenv())
+Config = environ
 
 
 	
