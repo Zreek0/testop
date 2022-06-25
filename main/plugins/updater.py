f6a07f8176390cf869103daecad2cc9a6a3d8220
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-06-26 02:25:18 +0530

    Update updater.py

--- main/plugins/updater.py
+++ main/plugins/updater.py
@@ -4,7 +4,7 @@ from .pyexec import bash
 from . import *
 
 @bot.on(events.NewMessage("/update$", from_users=SUDOS))
-async update_soft(event):
+async def update_soft(event):
 	e = await event.reply("`Fast Soft Updating...`")
 	ok, err = await bash("git pull -f && pip install -r requirements.txt")
 	os.execl(sys.executable, "python3", "-m", "main")
