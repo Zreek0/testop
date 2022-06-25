952abc3b3146098fde5e51aa7387fd4b0251bef7
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-06-26 02:35:29 +0530

    Update updater.py

--- main/plugins/updater.py
+++ main/plugins/updater.py
@@ -4,7 +4,7 @@ from telethon.sync import events
 from .pyexec import bash
 from . import *
 
-@bot.on(events.NewMessage("/update$", from_users=SUDOS))
+@bot.on(events.NewMessage(pattern="/update$", from_users=SUDOS))
 async def update_soft(event):
 	e = await event.reply("`Fast Soft Updating...`")
 	ok, err = await bash("git pull -f && pip install -r requirements.txt")
