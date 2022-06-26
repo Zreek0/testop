81b0c56e05f7072e991fe911dcb00bb48df068fa
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-06-26 15:09:48 +0530

    Update utils.py

--- main/plugins/utils.py
+++ main/plugins/utils.py
@@ -4,6 +4,19 @@ from telethon.sync import events
 from telethon.errors.rpcerrorlist import MessageTooLongError
 from . import *
 
+def hb(size):
+    if not size:
+        return "0 B"
+    for unit in ["", "K", "M", "G", "T"]:
+        if size < 1024:
+            break
+        size /= 1024
+    if isinstance(size, int):
+        size = f"{size}{unit}B"
+    elif isinstance(size, float):
+        size = f"{size:.2f}{unit}B"
+    return size
+
 @bot.on(events.NewMessage(pattern="/ls ?(.*)", from_users=SUDOS, incoming=True))
 async def listdirectory(event):
     files = event.pattern_match.group(1).strip()
