f7e3aefda4b549136a50335030301561c7666008
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-06-26 10:52:37 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -6,5 +6,5 @@ import time
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.DEBUG)
 
-bot = TelegramClient("AutoAnime", Config.get("API_ID"), Config.get("API_HASH")).start(bot_token=Config.get("BOT_TOKEN"))
+bot = TelegramClient("AutoAnime", Config.get("API_ID"), Config.get("API_HASH"))
 SUDOS = 5038395271, 5370531116
