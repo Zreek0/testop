4c02e582809eb046a901635bddb544a1858b0530
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-06-26 10:46:10 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -13,8 +13,10 @@ for name in files:
 		p = Path(f.name)
 		plugin_name = p.stem
 		load_plugins(plugin_name.replace(".py", ""))
-
-asyncio.run(bot.disconnect())
+try:
+    asyncio.run(bot.disconnect())
+excep Exception as e:
+    print(e)
 print("• Successfully Deployed Bot")
 if __name__ == "__main__":
 	bot.run_until_disconnected()
