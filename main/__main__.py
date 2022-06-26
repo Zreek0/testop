1d7af2dd1d0a6d62ec95c3bc3b5b24856ab6c7e7
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-06-26 10:44:05 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -4,7 +4,7 @@ from .loader import load_plugins
 from . import bot
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.Debug)
+                    level=logging.debug)
 
 path = "./main/plugins/*py"
 files = glob.glob(path)
