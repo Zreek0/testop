e1dfd1e170c4a97bd45dbb6cb8c19e26a1555747
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-06-26 11:11:19 +0530

    Update loader.py

--- main/loader.py
+++ main/loader.py
@@ -4,11 +4,14 @@ import importlib
 from pathlib import Path
 
 def load_plugins(plugin_name):
-    path = Path(f"main/plugins/{plugin_name}.py")
-    name = "main.plugins.{}".format(plugin_name)
-    spec = importlib.util.spec_from_file_location(name, path)
-    load = importlib.util.module_from_spec(spec)
-    load.logger = logging.getLogger(plugin_name)
-    spec.loader.exec_module(load)
-    sys.modules["main.plugins." + plugin_name] = load
-    print("main has Imported " + plugin_name)
+    if plugin_name.starswith("__"):
+    	pass
+    else:
+    	path = Path(f"main/plugins/{plugin_name}.py")
+    	name = "main.plugins.{}".format(plugin_name)
+    	spec = importlib.util.spec_from_file_location(name, path)
+    	load = importlib.util.module_from_spec(spec)
+    	load.logger = logging.getLogger(plugin_name)
+    	spec.loader.exec_module(load)
+    	sys.modules["main.plugins." + plugin_name] = load
+    	logging.info("main has Imported " + plugin_name)
