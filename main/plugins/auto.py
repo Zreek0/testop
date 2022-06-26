fb223ebc23ddaf7c9b73e0aa65b29151122c29f7
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-06-26 17:47:32 +0530

    Create auto.py

---
+++ main/plugins/auto.py
@@ -0,0 +1,26 @@
+import os, shutil, logging
+
+from ..fast_telethon import downloader, uploader
+from .pyexec import bash
+from telethon.sync import events
+from . import *
+
+logs = logging.getLogger(__name__)
+@bot.on(events.NewMessage(incoming=True, func=lambda e: e.sender_id == 5449257832))
+async def nyaa(event):
+	if event.file:
+		d = await downloader(bot, event)
+		os.rename(event.file.name, f"{event.id}.mkv")
+		ename = event.file.name.replace("[SubsPlease]", "[@Auto_Anime]").split("[ ")[0] + ".mkv"
+		cmd = f"""ffmpeg -i "{event.id}.mkv" -vf subtitles=input.mkv -map 0 -c:v libx265 -crf 28 "{ename}" && echo Done"""
+		thumb = await event.download_media(thumb=-1)
+		ok, err = await bash(cmd)
+		if ok:
+			u = await uploader(bot, ename)
+			pname = ename.replace(" (720p).mkv", "")
+			await bot.send_file(-1001448819386, u, thumb=thumb, caption=f"**✦ Name:** `{pname}`\n**✦ Quality :** `720p`")
+		else:
+			logs.info(err)
+		os.remove(f"{event.id}.mkv")
+		os.remove(ename)
+			
