da60a26b617da0c6ed099158abf7dc187e3ddb1b
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-06-26 19:26:14 +0530

    Update auto.py

--- main/plugins/auto.py
+++ main/plugins/auto.py
@@ -11,13 +11,14 @@ async def nyaa(event):
 	if event.media and event.message.text.startswith("[SubsPlease]"):
 		d = await downloader(bot, event)
 		os.rename(event.file.name, f"{event.id}.mkv")
-		ename = event.file.name.replace("[SubsPlease]", "[@Auto_Anime]").split("[ ")[0] + ".mkv"
+		ename = event.file.name.replace("[SubsPlease]", "[@Auto_Anime]")
+                ename = ename.split(" [")[0] + ".mkv"
 		cmd = f"""ffmpeg -i "{event.id}.mkv" -vf subtitles={event.id}.mkv -map 0 -c:v libx265 -crf 28 "{ename}" && echo Done"""
 		thumb = await event.download_media(thumb=-1)
 		ok, err = await bash(cmd)
 		if ok:
 			u = await uploader(bot, ename)
-			pname = ename.replace(" (720p).mkv", "")
+			pname = ename.replace("[@Auto_Anime]", "").replace(" (720p).mkv")
 			await bot.send_file(-1001448819386, u, thumb=thumb, caption=f"**✦ Name:** `{pname}`\n**✦ Quality :** `720p`")
 		else:
 			logs.info(err)
