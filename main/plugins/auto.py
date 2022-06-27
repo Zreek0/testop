7d9be01b4155726e50c887fbd562fe30b7569a5c
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-06-27 18:01:06 +0530

    Update auto.py

--- main/plugins/auto.py
+++ main/plugins/auto.py
@@ -27,14 +27,14 @@ async def nyaa(event):
 	if event.media and event.message.text.startswith("[SubsPlease]"):
 		d = await download_file(event.client, event.document, open(event.file.name, "wb"), progress_callback=progress_bar)
 		os.rename(event.file.name, f"{event.id}.mkv")
-		ename = event.file.name.replace("[SubsPlease]", "[@Auto_Anime]")
+		ename = event.file.name.replace("[SubsPlease]", "[@Ongoing_Anime_Uploads]")
 		ename = ename.split(" [")[0] + ".mkv"
-		cmd = f"""ffmpeg -i "{event.id}.mkv" -vf subtitles={event.id}.mkv -map 0 -c:v libx265 -crf 28 "{ename}" && echo Done"""
+		cmd = f"""ffmpeg -i "{event.id}.mkv" -vf subtitles={event.id}.mkv -map 0 -c:v libx264 -crf 28 "{ename}" && echo Done"""
 		thumb = await event.message.download_media(thumb=-1)
 		ok, err = await bash(cmd)
 		if ok:
 			u = await upload_file(event.client, open(ename, "rb"), progress_callback=progress_bar)
-			pname = ename.replace(" [@Auto_Anime]", "").replace(" (720p).mkv", "")
+			pname = ename.replace("[@Ongoing_Anime_Uploads] ", "").replace(" (720p).mkv", "")
 			await bot.send_file(-1001448819386, u, thumb=thumb, caption=f"**✦ Name:** `{pname}`\n**✦ Quality :** `720p`")
 		else:
 			logging.getLogger(__name__).info(err)
