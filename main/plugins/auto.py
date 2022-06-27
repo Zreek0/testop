353d7d6f72063e43b768a0660a66cd6efc682c00
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-06-27 16:44:41 +0530

    Update auto.py

--- main/plugins/auto.py
+++ main/plugins/auto.py
@@ -1,15 +1,33 @@
-import os, shutil, logging
+import os, shutil, logging, tim
 
 from ..fast_telethon import download_file, upload_file, uploader
 from .pyexec import bash
 from telethon.sync import events
 from . import *
 
+class Timer:
+    def __init__(self, time_between=2):
+        self.start_time = time.time()
+        self.time_between = time_between
+
+    def can_send(self):
+        if time.time() > (self.start_time + self.time_between):
+            self.start_time = time.time()
+            return True
+        return False
+
 logs = logging.getLogger(__name__)
 @bot.on(events.NewMessage(from_users=5038395271))
 async def nyaa(event):
+	timer = Timer()
+	msg = await bot.get_messages(-1001718753693, ids=1314)
+	async def progress_bar(current, total):
+		timer = Timer()
+		if timer.can_send():
+			await msg.edit("{} {}%".format(type_of, current * 100 / total))
 	if event.media and event.message.text.startswith("[SubsPlease]"):
-		d = await download_file(event.client, event.document, open(event.file.name, "wb"))
+		d = await download_file(event.client, event.document, open(event.file.name, "wb"), progress_callback=progress_bar)
+		await msg.edit("✦ **Status**: `Idle!`")
 		os.rename(event.file.name, f"{event.id}.mkv")
 		ename = event.file.name.replace("[SubsPlease]", "[@Auto_Anime]")
 		ename = ename.split(" [")[0] + ".mkv"
@@ -17,7 +35,8 @@ async def nyaa(event):
 		thumb = await event.message.download_media(thumb=-1)
 		ok, err = await bash(cmd)
 		if ok:
-			u = await uploader(bot, ename)
+			u = await upload_file(event.client, open(ename, "rb"), progress_callback=progress_bar)
+			await msg.edit("✦ **Status**: `Idle!`")
 			pname = ename.replace(" [@Auto_Anime]", "").replace(" (720p).mkv", "")
 			await bot.send_file(-1001448819386, u, thumb=thumb, caption=f"**✦ Name:** `{pname}`\n**✦ Quality :** `720p`")
 		else:
