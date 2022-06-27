31dea05ef841d0c14841c20b6ec6d6cfed1ceb01
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-06-28 00:46:17 +0530

    Update auto.py

--- main/plugins/auto.py
+++ main/plugins/auto.py
@@ -34,7 +34,7 @@ async def nyaa(event):
 		ok, err = await bash(cmd)
 		if ok:
 			u = await upload_file(event.client, open(ename, "rb"), progress_callback=progress_bar)
-			pname = ename.replace("[@Ongoing_Anime_Uploads] ", "").replace(" (720p).mp4", "")
+			pname = ename.replace("[@Ongoing_Seasonal_Anime] ", "").replace(" (720p).mp4", "")
 			await bot.send_file(-1001448819386, u, thumb=thumb, caption=f"**✦ Name :** `{pname}`\n**✦ Quality :** `720p`", supports_streaming=True)
 		else:
 			logging.getLogger(__name__).info(err)
