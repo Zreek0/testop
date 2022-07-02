0013ab1175bbd63bd520de21677a02a4e775b375
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-07-03 00:22:27 +0530

    Update mid.py

--- main/plugins/mid.py
+++ main/plugins/mid.py
@@ -2,7 +2,7 @@ from ..sql.globals import gvarstatus, addgvar, delgvar
 from . import *
 from telethon import events
 
-@bot.on(events.NewMessage(chats=-1001606385356))
+@bot.on(events.NewMessage(chats="adult_mangas"))
 async def _(event):
 	id = gvarstatus("MID")
 	if event.text and "releasing" in event.text.lower() and str(event.id) != str(id):
