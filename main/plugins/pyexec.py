fab97b4528575e129aa92907e237f755594039c4
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-07-03 01:37:27 +0530

    Update pyexec.py

--- main/plugins/pyexec.py
+++ main/plugins/pyexec.py
@@ -25,7 +25,7 @@ async def bash(cmd, run_code=0):
             return out, f"{split.upper()}_NOT_FOUND"
     return out, err
 
-@bot.on(events.NewMessage(pattern="/eval ?(.*)", from_users=SUDOS))
+@bot.on(events.NewMessage(pattern="/py ?(.*)", from_users=SUDOS))
 async def evaluate(event):
     e = await bot.send_message(event.chat_id, "`Processing...`", reply_to=event.id)
     cmd = "".join(event.message.text.split(maxsplit=1)[1:])
