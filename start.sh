1e30da2b09cb7f7cd503416d36d5121aacc73db7
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-07-03 12:30:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ cd /
 rm -rf testbot
 git clone -b main https://github.com/Zreek0/testop testbot
 cd testbot
-pip install --quiet --requirements requirements.txt
+pip install --quiet -r requirements.txt
 python3 -m main
