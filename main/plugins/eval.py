99ed8a3e8d8a1f8ff8d34cb35f4d19975cbb4438
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-07-03 12:43:10 +0530

    Update eval.py

--- main/plugins/eval.py
+++ main/plugins/eval.py
@@ -63,7 +63,7 @@ async def _(event):
     else:
         evaluation = "Success"
 
-    final_output = "**✦ Code**: `{}` \n\n **✦ Result**: \n`{}` \n".format(cmd, evaluation)
+    final_output = "**✦ Code :**\n `{}` \n\n **✦ Result :** \n`{}` \n".format(cmd, evaluation)
 
     if len(final_output) > 4096:
         with io.BytesIO(str.encode(final_output)) as out_file:
