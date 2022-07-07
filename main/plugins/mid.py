ef7ac5454f90ed81f945782e2fe4f9ae0728c1ad
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-07-07 20:25:31 +0530

    Update mid.py

--- main/plugins/mid.py
+++ main/plugins/mid.py
@@ -1,9 +1,12 @@
 from ..sql.globals import gvarstatus, addgvar, delgvar
 from . import *
+from .uws import get_names
 from telethon import events
 
 @bot.on(events.NewMessage(chats="adult_mangas"))
-async def _(event):
-	id = gvarstatus("MID")
-	if event.text and "releasing" in event.text.lower() and str(event.id) != str(id):
+async def _mid(event):
+	if event.text and "releasing" in event.message.lower():
 		addgvar("MID", event.id)
+		names = await get_names()
+		addgvar("GET_NAMES", names)
+	
