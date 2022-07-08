8d6c0336cbc855d41026043dab08da00956bc564
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-07-08 18:05:19 +0530

    Update ongoing.py

--- main/plugins/ongoing.py
+++ main/plugins/ongoing.py
@@ -5,7 +5,6 @@ import cloudscraper
 from .auws import *
 from . import *
 
-pnames = get_names()
 
 def get_link(link, cloud=None):
 	if cloud:
@@ -22,6 +21,7 @@ def get_link(link, cloud=None):
 
 @bot.on(admin_cmd("uws ?(.*)", allow_sudo=True))
 async def _uws(event):
+        pnames = await get_names()
 	input_str = event.pattern_match.group(1)
 	splited = str(input_str).split(" ")
 	mess = await eor(event, "`Processing...`")
