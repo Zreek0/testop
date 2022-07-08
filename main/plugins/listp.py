289f8ffb91d1c248beeaaee1eb27dc9f3f76d281
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-07-08 17:48:44 +0530

    Update listp.py

--- main/plugins/listp.py
+++ main/plugins/listp.py
@@ -6,19 +6,19 @@ from .auws import get_ids
 ids = get_ids()
 @bot.on(events.NewMessage(pattern="/listp ?(.*)", from_users=SUDOS))
 async def getplist(event):
-	e = await bot.send_message(event.chat_id, "`Processing...`", reply_to=event.id)
+	e = await eor(event, "`Processing...`")
 	input_str = event.pattern_match.group(1)
-	mess = await bot.get_messages("adult_mangas", ids=ids)
+	mess = await app.get_messages("adult_mangas", message_id=ids)
 	post = str()
 	n = 0
 	for m in mess:
-		if m and m.photo and m.text and "releasing" in m.message.lower():
-			name = m.message.split("\n")[0].split(" | ")[0]
-			links = re.findall("[^.]ttps://t.me/.*", m.text)[0].replace(")", "")
-			link = links.replace("[New Link]", "").replace(" ", "")
-			if input_str in ["-c", "--channel"]:
-				post += f"➤ [{name}]({link})\n"
+		if m.caption and "releasing" in m.caption.lower():
+			name = m.caption.split("\n")[0].split(" | ")[0]
+			url = m.caption_entities[-1].url
+			if input_str.strip() in ["-c", "--channel"]:
+				post += f"➤ [{name}]({url})\n"
 			else:
 				n += 1
-				post += f"{n-1}.] [{name}]({link})\n"
-	await e.edit(post, link_preview=False)
+				post += f"{n-1}.] [{name}]({url})"
+	await eor(e, post)
+	
