efe918279c17cb82a3c6f7552ead6abf0cc56775
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-07-08 13:56:41 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,6 +1,7 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 from telethon import events
+from pyrogram import Client
 from config import Config
 import logging
 import time
@@ -9,3 +10,5 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.INFO)
 bot = TelegramClient("Assistant-Bot", Config.get("API_ID"), Config.get("API_HASH")).start(bot_token=Config.get("BOT_TOKEN"))
 SUDOS = 5038395271, 5370531116, 5074055497
+app = Client("Assist, Config.get("API_ID"), Config.get("API_HASH"), bot_token=Config.get("BOT_TOKEN"))
+app.start()
