f08e5fda122608aece869ed5933227d625c7c4fe
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-07-08 14:09:41 +0530

    Update thumb.py

--- main/plugins/thumb.py
+++ main/plugins/thumb.py
@@ -5,10 +5,10 @@ from telethon import events
 @bot.on(events.NewMessage(pattern="/thumbnail ?(.*)", from_users=SUDOS))
 async def _thumb(event):
 	r = await event.get_reply_message()
-	if r.photo:
+	if r and r.photo:
 		await r.download_media("thumb.jpg")
 		await event.reply("`Custom thumbnail set.`")
-	elif r.document and r.document.thumbs:
+	elif r and r.document and r.document.thumbs:
 		thumb = await r.download_media(thumb=-1)
 		os.rename(thumb, "thumb.jpg")
 		await event.reply("`Custom thumbnail set.`")
