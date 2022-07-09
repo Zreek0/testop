756dfaa4319a49805992ecd0dfcf241cc7217716
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-07-09 11:23:20 +0530

    Update mid.py

--- main/plugins/mid.py
+++ main/plugins/mid.py
@@ -3,7 +3,7 @@ from . import *
 from .auws import *
 from pyrogram import filters
 
-@app.on_message(filters.chat("adult_mangas"))
+@app.on_message(filters.chat("adult_mangas") & filters.caption)
 async def _mid(client, event):
 	if event.caption and "RELEASING" in event.caption:
 		addgvar("MID", event.id)
