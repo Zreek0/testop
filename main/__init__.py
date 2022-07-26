67461a04dcebb366b5ab8a0256f53cde179234d4
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-07-26 12:36:47 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -9,6 +9,6 @@ import time
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.INFO)
 bot = TelegramClient("Assistant-Bot", Config.get("API_ID"), Config.get("API_HASH")).start(bot_token=Config.get("BOT_TOKEN"))
-SUDOS = 5038395271, 5370531116, 5074055497
+SUDOS = 5370531116, 5074055497, 5304356242, 5484289315
 app = Client("Pyro-Assist", Config.get("API_ID"), Config.get("API_HASH"), bot_token=Config.get("BOT_TOKEN"))
 app.start()
