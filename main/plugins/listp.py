0f667a8daef6b0e773468cae82753e8feaaf9b1a
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-07-30 13:02:02 +0530

    Update listp.py

--- main/plugins/listp.py
+++ main/plugins/listp.py
@@ -1,15 +1,14 @@
 import re
 from . import *
 from telethon import events
-from .ongoing import pnames
 from ..sql.globals import gvarstatus
 
 def get_plist():
 	data = dict()
 	ids1 = list(range(2, 201))
 	ids2 = list(range(201, int(gvarstatus("MID"))))
-	mess1 = app.get_messages("adult_mangas", message_ids=ids1)
-	mess2 = app.get_messages("adult_mangas", message_ids=ids2)
+	mess1 = await app.get_messages("adult_mangas", message_ids=ids1)
+	mess2 = await app.get_messages("adult_mangas", message_ids=ids2)
 	mess = mess1 + mess2
 	for m in mess:
 		if m.caption and m.caption_entities and "releasing" in m.caption.lower():
@@ -23,20 +22,13 @@ plist = get_plist()
 async def getplist(event):
 	e = await eor(event, "`Processing...`")
 	input_str = event.pattern_match.group(1)
-	post = str()
+	post = ""
 	n = 0
-	ids1 = list(range(2, 201))
-	ids2 = list(range(201, int(gvarstatus("MID"))))
-	mess1 = await app.get_messages("adult_mangas", message_ids=ids1)
-	mess2 = await app.get_messages("adult_mangas", message_ids=ids2)
-	mess = mess1 + mess2
-	for m in mess:
-		if m.caption and m.caption_entities and "releasing" in m.caption.lower():
-			name = m.caption.split("\n")[0].split(" | ")[0]
-			url = m.caption_entities[-1].url
-			if input_str.strip() in ["-c", "--channel"]:
-				post += f"➤ [{name}]({url})\n"
-			else:
-				n += 1
-				post+= f"{n-1}.] [{name}]({url})\n"
-	await eor(e, post)
+	for i in pnames:
+		if input_str in ["-c", "--channel", "-p", "--post"]:
+			post += f"➤ [{i}]({plist[i]})\n"
+		else:
+			n += 1
+			post += f"{n-1}.] [{i}]({plist[i]})\n"
+	await eor(event, post)
+	
