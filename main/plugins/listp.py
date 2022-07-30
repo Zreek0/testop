6036b32978444aaddfedd97514ffcdefe73920a6
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-07-30 12:54:44 +0530

    Update listp.py

--- main/plugins/listp.py
+++ main/plugins/listp.py
@@ -3,6 +3,21 @@ from . import *
 from telethon import events
 from ..sql.globals import gvarstatus
 
+def get_plist():
+	data = dict()
+	ids1 = list(range(2, 201))
+	ids2 = list(range(201, int(gvarstatus("MID"))))
+	mess1 = await app.get_messages("adult_mangas", message_ids=ids1)
+	mess2 = await app.get_messages("adult_mangas", message_ids=ids2)
+	mess = mess1 + mess2
+	for m in mess:
+		if m.caption and m.caption_entities and "releasing" in m.caption.lower():
+			name = m.caption.split("\n")[0].split(" | ")[0].strip()
+			link = m.caption_entities[-1].url
+			data[name] = link
+	return data
+
+plist = get_plist()
 @bot.on(events.NewMessage(pattern="/listp ?(.*)", from_users=SUDOS))
 async def getplist(event):
 	e = await eor(event, "`Processing...`")
