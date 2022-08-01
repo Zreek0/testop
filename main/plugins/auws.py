d8760eade9496a8db60cc716c95f29eb2b4b1191
Zreek0 <108022789+Zreek0@users.noreply.github.com>
2022-08-01 15:22:38 +0530

    Update auws.py

--- main/plugins/auws.py
+++ main/plugins/auws.py
@@ -53,7 +53,7 @@ def create_pdf(path, images: list):
 
 	pdf.save()
 
-async def post_ws(link, name, chapter, class_="wp-manga-chapter-img", src="src"):
+async def post_ws(link, name, chapter, class_="wp-manga-chapter-img", src="src", **args):
 	chno = str(chapter)
 	chno = chno.replace("-", ".")
 	pdfname = f"./Chapter {chno} {name}" + " @Adult_Mangas.pdf"
@@ -71,7 +71,7 @@ async def post_ws(link, name, chapter, class_="wp-manga-chapter-img", src="src")
 		r = requests.get(link)
 		r.raise_for_status()
 	soup = BeautifulSoup(r.text, "html.parser")
-	image_links = soup.find_all("img", class_)
+	image_links = soup.find_all("img", class_, **args)
 	n = 0
 	images = []
 	for i in image_links:
